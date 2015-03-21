d51996ead0ef3f2e16828bd6b53b1a03626c3c03
Denis Smolov <densmolov@gmail.com>
2015-03-22 01:26:29 +0300

    Some changes

---
+++ code/js/app.js
@@ -0,0 +1,66 @@
+(function () {
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function () {
+        this.products = stones;
+    });
+
+    app.controller('PanelController', function () {
+        this.tab = 3;
+
+        this.selectTab = function(setTab){
+            this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        };
+    });
+
+    var stones = [
+        {
+            name: 'Arkenstone',
+            price: 100,
+            description: 'Tra-ta-ta...',
+            image: "images/Arkenstone.png",
+            canPurchase: true,
+            reviews: [
+                {
+                    stars: 3,
+                    body: "At least you should give it a try.",
+                    author: "anonymous@mail.su"
+                },
+                {
+                    stars: 5,
+                    body: "Awesome!",
+                    author: "joe@mail.su"
+                },
+                {
+                    stars: 1,
+                    body: "Really awful, sorry.",
+                    author: "TheRagChewer@mail.su"
+                }
+            ]
+        },
+        {
+            name: 'Dragon Eye',
+            price: 120,
+            description: 'Ogo-go...',
+            image: "images/dragon_eye.jpg",
+            canPurchase: false,
+            reviews: [
+                {
+                    stars: 3,
+                    body: "Never again.",
+                    author: "tyrtyr@mail.su"
+                },
+                {
+                    stars: 2,
+                    body: "Oh no!..",
+                    author: "ape@mail.su"
+                }
+            ]
+        }
+    ]   //var stones
+
+})();
