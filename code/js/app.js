3b69af996a90de2ceab2ba3a019b295bc93e3d61
Denis Smolov <densmolov@gmail.com>
2015-03-22 15:45:50 +0300

    Final step

--- code/js/app.js
+++ code/js/app.js
@@ -1,53 +1,16 @@
 (function () {
-    var app = angular.module('store', []);
+    var app = angular.module('store', ['store-products']);
 
-    app.controller('StoreController', function () {
-        this.products = [
-        {
-            name: 'Arkenstone',
-            price: 100,
-            description: 'Tra-ta-ta...',
-            image: "images/Arkenstone.png",
-            canPurchase: true,
-            reviews: [
-                {
-                    stars: 3,
-                    body: "At least you should give it a try.",
-                    author: "anonymous"
-                },
-                {
-                    stars: 5,
-                    body: "Awesome!",
-                    author: "joe@mail.su"
-                },
-                {
-                    stars: 1,
-                    body: "Really awful, sorry.",
-                    author: "TheRagChewer"
-                }
-            ]
-        },
-        {
-            name: 'Dragon Eye',
-            price: 120,
-            description: 'Ogo-go...',
-            image: "images/dragon_eye.jpg",
-            canPurchase: false,
-            reviews: [
-                {
-                    stars: 3,
-                    body: "Never again.",
-                    author: "tyrtyr"
-                },
-                {
-                    stars: 2,
-                    body: "Oh no!..",
-                    author: "ape@mail.su"
-                }
-            ]
-        }
-    ];
-    });
+    // Dependency Injection
+    app.controller('StoreController', ['$http', function($http) {
+        var store = this;
+        store.products = [];
+
+        // Service
+        $http.get('./data/products.json').success(function(data){
+            store.products = data;
+        });
+    }]);
 
     /*app.controller('PanelController', function () {
         this.tab = 3;
@@ -66,30 +29,6 @@
             this.review = {};
         }
     });*/
-
-    app.directive('productTitle', function(){
-        return {
-            restrict: 'E',
-            templateUrl: 'templates/product-title.html'
-        };
-    });
-
-    app.directive('productPanels', function(){
-        return {
-            restrict: 'E',
-            templateUrl: 'templates/product-panels.html',
-            controller: function () {
-                this.tab = 3;
-                this.selectTab = function(setTab){
-                    this.tab = setTab;
-                };
-                this.isSelected = function(checkTab){
-                    return this.tab === checkTab;
-                };
-            },
-            controllerAs: 'panelCtrl'
-        };
-    });
 
     app.directive('reviewForm', function(){
         return {
